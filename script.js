806a1b7dd4bb2f9a110d41eb9c37de1cd7c4b888
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-03-16 17:16:55 +0100

    bonus col button

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ const app = new Vue(
         el: "#app",
         data :{
             message : "ciao", 
-            url : "https://cdn.bhdw.net/im/league-of-legends-lol-dark-spirit-morgana-sfondo-64398_w635.jpg",
-            classe: "containerImg"  
+        },
+        methods:{
+            immagine: function(){
+                document.getElementById("contenitoreImg").innerHTML=`<img src="https://cdn.bhdw.net/im/league-of-legends-lol-dark-spirit-morgana-sfondo-64398_w635.jpg" alt="">`
+            }
         }
     }
 )
