59d191e348044780becd9cc49966d3671dab1b7e
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-03-16 16:21:33 +0100

    .

--- script.js
+++ script.js
@@ -6,7 +6,16 @@ const app = new Vue(
     {
         el: "#app",
         data :{
-            message : "ciao"
+            message : "ciao",    
+        }
+    }
+)
+
+const appTwo = new Vue(
+    {
+        el : "#appTwo",
+        data : {
+            url: "https://cdn.bhdw.net/im/league-of-legends-lol-dark-spirit-morgana-sfondo-64398_w635.jpg" 
         }
     }
 )
